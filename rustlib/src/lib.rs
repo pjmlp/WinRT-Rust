2f4f6703d350411f9ab6afbcb54c8ce9d470a60e
pjmlp <pjmlp@progtools.org>
2016-02-22 23:43:38 +0100

    Updated the function on how to hande 16 bytes strings.

--- rustlib/src/lib.rs
+++ rustlib/src/lib.rs
@@ -1,4 +1,15 @@
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+
+fn to_wchar(str : &str) -> *const u16 {
+    let v : Vec<u16> =
+            OsStr::new(str).encode_wide(). chain(Some(0).into_iter()).collect();
+
+    v.as_ptr()
+}
+
+
 #[no_mangle]
-pub extern "C" fn say_hello() -> *const u8 {
-    "Hello from Rust\0".as_ptr()
+pub extern "C" fn say_hello() -> *const u16 {
+    to_wchar("Hello from Rust\0")
 }
